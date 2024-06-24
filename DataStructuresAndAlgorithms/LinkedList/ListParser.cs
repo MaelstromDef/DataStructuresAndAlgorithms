574dbf6f155be4eea971328cf0e36c2adb7f7fcb
Aaron Huggins <achuggi1@asu.edu>
2024-06-24 12:37:55 -0700

    Singly Linked Lists
    Create a singly linked list in c#.

---
+++ DataStructuresAndAlgorithms/LinkedList/ListParser.cs
@@ -0,0 +1,133 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Runtime.CompilerServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class ListParser
+    {
+        static SinglyLinkedList list = new SinglyLinkedList();
+        static string[] options =
+        {
+            "1. Print list." ,
+            "2. Add a node.",
+            "3. Remove a node.",
+            "4. Clear list.",
+            "5. Exit."
+        };
+
+        public static void Main()
+        {
+            Console.WriteLine("Hello World!");
+            while(HandleInput(GatherInput()));
+        }
+
+        /// <summary>
+        /// Prints the available program options and retrieves the user's selection.
+        /// </summary>
+        /// <returns>The user's selection.</returns>
+        static int GatherInput()
+        {
+            Console.WriteLine("Please select an option:");
+            Array.ForEach(options, Console.WriteLine);
+
+            string line = Console.ReadLine() ?? string.Empty;
+            try {
+                int selection = int.Parse(line);
+                if (selection < 1 || selection > options.Length) throw new ArgumentOutOfRangeException(nameof(selection));
+
+                return selection;
+            } catch
+            {
+                Console.WriteLine("Please input a valid number between 1 and " + options.Length + ".");
+                return GatherInput();
+            }
+        }
+
+        /// <summary>
+        /// Performs an action based on a given selection.
+        /// </summary>
+        /// <param name="selection">User's action selection.</param>
+        /// <returns>Whether to continue the program or not.</returns>
+        static bool HandleInput(int selection)
+        {
+            switch (selection)
+            {
+                case 1:
+                    PrintList();
+                    break;
+                case 2:
+                    AddNode();
+                    break;
+                case 3:
+                    RemoveNode();
+                    break;
+                case 4:
+                    ClearList();
+                    break;
+                default:
+                    return false;
+            }
+
+            Console.WriteLine("\n");
+            return true;
+        }
+
+        #region Actions
+
+        /// <summary>
+        /// Calls the list to print itself.
+        /// </summary>
+        static void PrintList()
+        {
+            list.PrintList();
+        }
+
+        /// <summary>
+        /// Gathers what node to add and calls the list to add it.
+        /// </summary>
+        static void AddNode()
+        {
+            Console.WriteLine("Please enter an integer to add.");
+            try
+            {
+                int number = int.Parse(Console.ReadLine() ?? string.Empty);
+                list.AddNode(number);
+            }
+            catch
+            {
+                Console.WriteLine("Invalid integer.");
+            }
+        }
+
+        /// <summary>
+        /// Gathers what node to remove and calls the list to remove it.
+        /// </summary>
+        static void RemoveNode()
+        {
+            Console.WriteLine("Please enter an integer to remove.");
+            try
+            {
+                int number = int.Parse(Console.ReadLine() ?? string.Empty);
+                list.RemoveNode(number);
+            }
+            catch
+            {
+                Console.WriteLine("Invalid integer.");
+            }
+        }
+
+        /// <summary>
+        /// Calls the list to clear itself.
+        /// </summary>
+        static void ClearList()
+        {
+            list.ClearList();
+        }
+
+        #endregion
+    }
+}
---
+++ DataStructuresAndAlgorithms/LinkedList/SinglyLinkedList.cs
@@ -0,0 +1,104 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LinkedList
+{
+    public class SinglyLinkedList
+    {
+        ListNode? head;
+
+        /// <summary>
+        /// Traverses the list and prints the data of each item.
+        /// </summary>
+        public void PrintList()
+        {
+            ListNode? temp = head;
+            while (temp != null)
+            {
+                Console.Write(temp.data + " ");
+                temp = temp.next;
+            }
+        }
+
+        /// <summary>
+        /// Adds a number to the list. List will always be in ascending order.
+        /// </summary>
+        /// <param name="number">Number to add.</param>
+        public void AddNode(int number)
+        {
+            ListNode node = new ListNode() { data = number };   // Create node
+
+            // Head
+            if(node.data <= head?.data || head == null)
+            {
+                node.next = head;
+                head = node;
+                return;
+            }
+
+            // Body
+            ListNode temp = head;
+            while (temp!.next != null)
+            {
+                if(node.data <= temp.next.data)
+                {
+                    node.next = temp.next;
+                    temp.next = node;
+                    return;
+                }
+
+                temp = temp.next!;
+            }
+
+            // Tail
+            temp.next = node;
+        }
+
+        /// <summary>
+        /// Removes a number from the list. 
+        /// 
+        /// If multiple instances of the number exist, only the first instance of the number is removed.
+        /// </summary>
+        /// <param name="number">Number to remove.</param>
+        public void RemoveNode(int number)
+        {
+            // Head
+            if (head?.data == number)
+            {
+                head = head.next;
+                return;
+            }
+
+            // Body
+            ListNode? temp = head;
+            while(temp != null)
+            {
+                if (temp.next?.data == number)
+                {
+                    temp.next = temp.next.next;
+                    return;
+                }
+            }
+        }
+
+        /// <summary>
+        /// Clears the list.
+        /// </summary>
+        public void ClearList()
+        {
+            head = null;
+            return;
+
+            // Garbage collection is automatic in C#, but this is a sort of pseudocode for languages w/o a garbage collector.
+            ListNode? temp = head;
+            while (temp != null)
+            {
+                temp = temp.next;
+                // Destroy(head);
+            }
+        }
+    }
+}
